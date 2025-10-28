8f813272112905b70752d58c8c437b4d742580d9
gallegos <gallegos.kenia@uabc.edu.mx>
2025-10-28 17:58:27 +0000

    agregacion de bibliotecas

--- p8/p8.asm
+++ p8/p8.asm
@@ -8,29 +8,29 @@ section	.text
 _start:
     
     ;a
-    in al, 80h
-    mov bx, 5
-    comp al, bx
-    jae .mayor
-    out 
-.mayor out 
+   ;n al, 80h
+   ;mv bx, 5
+   ;comp al, bx
+   ;jae .mayor
+   ;out 
+;mayor out 
+
+    ;b
+   ;in 
 
     ;c
     in al, 80h
-    mov cx, al
-.nuevo al,'*'
+    mov 
+.nuevo mov al, '*'
     out 0x92, al
     mov al, 10
     call putchar
     loop .nuevo
 
     ;d 
-    in al, 
-    out 
-    mov cx, 10 
-
-
-
+   ;in al, 
+   ;out 
+    ;ov cx, 10 
 
 mov eax, 1 ;final 
 int 0x80 
