6c610b420aebc70aea1af87479236831e4d4fae0
gallegos <gallegos.kenia@uabc.edu.mx>
2025-10-29 21:09:55 +0000

    segundo b

--- p8/p8.asm
+++ p8/p8.asm
@@ -1,5 +1,8 @@
 %include "../LIB/pc_iox.inc" 
 section .data
+msg db 'el numero es menor', 0xa, 0
+num db 'lo ingresado es un numero', 0xa, 0
+;ltr db 'lo ingresado es una letra', 0xa, 0
 
 section	.text
 
@@ -18,8 +21,30 @@ _start:
 .mayor mov al, 10
     call putchar
 
+    ;b
+    call getche 
+
+    cmp al, '0'
+    jb .fin
+    cmp al, '9'
+    jbe .numero ;jumpBelow
+
+    cmp al, 'A'
+    jb .fin
+    cmp al, 'Z'
+    jbe .letra
+
+.numero mov edx, num
+    call puts
+    jmp .fin
+
+.letra mov edx, msg
+    call puts
+    jmp .fin
+
+.fin mov al, 10
+    call putchar
+
 mov eax, 1 ;final 
 int 0x80 
 
-section .data
-msg db 'el numero es menor', 0xa, 0
